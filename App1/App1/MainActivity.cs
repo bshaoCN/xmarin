91ecd738b5ddb9a742e415238044d2b521ac527f
bshaoCN <bshao@163.com>
2017-11-13 09:34:52 +0800

    ..

--- App1/App1/MainActivity.cs
+++ App1/App1/MainActivity.cs
@@ -1,6 +1,9 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
 
 namespace App1
 {
@@ -10,7 +13,8 @@ namespace App1
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            MobileCenter.Start("e3f168f1-6102-4c5d-8c22-ac6ef651138a",
+                   typeof(Analytics), typeof(Crashes));
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
         }
